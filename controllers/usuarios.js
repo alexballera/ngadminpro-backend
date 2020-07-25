8601da579157cb8d2a7a7259ef553971f5bc13b4
Alex Ballera <alexballera@gmail.com>
2020-07-25 12:12:26 -0300

    Primera validacion de correo

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,3 +1,4 @@
+const { response } = require('express');
 const Usuario = require('../models/usuario');
 
 const getUsuarios = async(req, res) => {
@@ -10,21 +11,40 @@ const getUsuarios = async(req, res) => {
   })
 }
 
-const crearUsuarios = async(req, res) => {
+const crearUsuarios = async(req, res = response) => {
 
   // Destructuramos
   const { email, password, nombre } = req.body;
-
-  // Creamos una instancia de Usuario
-  const usuario = new Usuario(req.body);
-
-  // Guardamos en la base de datos
-  await usuario.save()
   
-  res.json({
-    ok: true,
-    usuario
-  })
+  try {
+
+    const existeEmail = await Usuario.findOne({email});
+
+    if (existeEmail) {
+      return res.status(400).json({
+        ok: false,
+        msg: 'El correo ya está registrado'
+      })
+    }
+
+    // Creamos una instancia de Usuario
+    const usuario = new Usuario(req.body);
+  
+    // Guardamos en la base de datos
+    await usuario.save()
+    
+    res.json({
+      ok: true,
+      usuario
+    })
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: 'Error inesperado... revisar logs'
+    })
+  } 
 }
 
 module.exports = {
